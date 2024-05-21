e643fa5c6eeed503a798345dcdce5710edc79ab7
wakhu shibero <wshibero@gmail.com>
2024-05-21 13:45:32 +0000

    variable and for

--- main.dart
+++ main.dart
@@ -1,3 +1,10 @@
 void main() {
   print('Hello World!');
+  print("hello my name is shibero");
+  var name = "Derrick";
+  print("name "+name);
+  var months = ["may", "june", "july", "august"];
+  for(var month in months){
+    print("the month is "+month);
+  }
 }
