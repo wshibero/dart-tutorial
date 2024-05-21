09e69f245803345eee2682f61f30d10c428713c7
wakhu shibero <wshibero@gmail.com>
2024-05-21 13:49:00 +0000

    while

--- main.dart
+++ main.dart
@@ -7,4 +7,10 @@ void main() {
   for(var month in months){
     print("the month is "+month);
   }
+  var year = 2016;
+  while(year < 2024){
+    var rem = 2024 - year;
+    print(rem.toString()+ " to 2024");
+    year++;
+  }
 }
